83dbc1ac497a255d3880e19d3cf5363b4ca39d5b
adi-pranata <adipranata77@gmail.com>
2025-05-17 00:06:21 +0800

    adjust main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/core/utilities/app_colors.dart';
-import 'package:todo_app/core/widgets/app_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,18 +8,9 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Demo',
-      home: Scaffold(
-        body: Center(
-          child: AppButton(
-            label: 'Next',
-            onPressed: () {},
-            icon: Icons.arrow_forward,
-            color: AppColor.secondary,
-          ),
-        ),
-      ),
+      
     );
   }
 } 
