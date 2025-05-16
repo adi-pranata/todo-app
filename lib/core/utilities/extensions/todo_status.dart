bf08ff53010aebbb4cef6d46b860fdbb5d2a5b9c
adi-pranata <adipranata77@gmail.com>
2025-05-17 01:30:49 +0800

    status badges

---
+++ lib/core/utilities/extensions/todo_status.dart
@@ -0,0 +1,33 @@
+import 'dart:ui';
+
+import 'package:todo_app/core/utilities/app_colors.dart';
+
+enum TodoStatus { open, done, overdue }
+
+extension TodoStatusExtension on TodoStatus {
+  String get label {
+    switch (this) {
+      case TodoStatus.done:
+        return 'DONE';
+      case TodoStatus.overdue:
+        return 'OVERDUE';
+      case TodoStatus.open:
+        return 'OPEN';
+    }
+  }
+
+  Color get backgroundColor {
+    switch (this) {
+      case TodoStatus.done:
+        return AppColor.alertGreen;
+      case TodoStatus.overdue:
+        return AppColor.alertRed;
+      case TodoStatus.open:
+        return AppColor.alertGrey;
+    }
+  }
+
+  Color get textColor {
+    return this == TodoStatus.open ? AppColor.primary20 : AppColor.white;
+  }
+}
